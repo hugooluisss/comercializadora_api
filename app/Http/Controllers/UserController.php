07f79b588071e4c74391d702bb10ed211e76a0cb
Hugo Santiago <hugooluisss@gmail.com>
2021-12-02 23:08:59 -0600

    I created Controller and route for users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $obj = User::create($request->all());
+        return response()->json($obj, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $obj = User::findOrFail($id);
+        $obj->update($request->all());
+
+        return response()->json($obj, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $obj = User::findOrFail($id);
+        $obj->delete();
+
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,4 +34,10 @@ Route::group([
         Route::get('logout', [AuthController::class, 'logout']);
         Route::get('get', [AuthController::class, 'getUser']);
     });
+});
+
+Route::group([
+    'middleware' => 'auth:api'
+  ], function() {
+    Route::resource('users', UserController::class)->except(['create', 'edit']);
 });
