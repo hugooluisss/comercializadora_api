61c27657ce89c8dc19ff80cc1ea79dd48fbdfc11
Hugo Santiago <hugooluisss@gmail.com>
2021-12-17 00:01:25 -0600

    Categorías de productos done

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $list = Category::get();
+        return $list;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $obj = Category::create($data);
+
+        return response()->json($obj, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $obj = Category::findOrFail($id);
+        $data = $request->all();
+
+        $obj->update($data);
+        return response()->json($obj, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $obj = Category::findOrFail($id);
+        $obj->delete();
+
+        return response(null, 204);
+    }
+}
