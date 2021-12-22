e2b98eff79c1fb55cddec65807a1bebf41d9c8aa
Hugo Santiago <hugooluisss@gmail.com>
2021-12-21 23:22:53 -0600

    I create a seeder for add categories and products to DB

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('categories')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        for($i = 0 ; $i < 5 ; $i++){
+            $idCategorie = DB::table('categories')->insertGetId([
+                'name' => "Category $i",
+                'description' => "Description of category $i"
+            ]);
+
+            for($iProduct = 0 ; $iProduct < 5 ; $iProduct++){
+                DB::table('products')->insert([
+                    'name' => "Product $iProduct",
+                    'category_id' => $idCategorie,
+                    'sku' => strtoupper(uniqid()),
+                    'name' => "Product $iProduct - Category $i",
+                    'description' => "Description of product $iProduct of category $i",
+                    'image' => ''
+                ]);
+            }
+        }
+    }
+}
