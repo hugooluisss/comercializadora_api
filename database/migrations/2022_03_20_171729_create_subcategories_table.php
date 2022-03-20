cb8e131775c4afdbc008779ce2b6469e8513b64b
Hugo Santiago <hugooluisss@gmail.com>
2022-03-20 11:44:42 -0600

    CI: Set new structur for categories

--- database/migrations/2022_03_20_171729_create_subcategories_table.php
+++ database/migrations/2022_03_20_171729_create_subcategories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateSubcategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->text('image')->nullable();
             $table->unsignedBigInteger('category_parent_id');
+            $table->unsignedBigInteger('category_child_id');
 
             $table->foreign('category_parent_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
-
-            $table->timestamps();
+            $table->foreign('category_child_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
@@ -33,6 +30,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('subcategories');
     }
 }
--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -18,25 +18,49 @@ class CategoriesSeeder extends Seeder
         
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('categories')->truncate();
+
+        $current = date('Y-m-d H:i:s');
+
+        $CategoryInit = DB::table('categories')->insertGetId([
+            'name' => "Categoría padre",
+            'category_parent_id' => 1,
+            'created_at' => $current,
+            'updated_at' => $current
+        ]);
+
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         for($i = 0 ; $i < 5 ; $i++){
-            $idCategorie = DB::table('categories')->insertGetId([
+            $idCategory = DB::table('categories')->insertGetId([
                 'name' => "Category $i",
                 'description' => "Description of category $i",
-                'image' => ''
+                'category_parent_id' => $CategoryInit,
+                'created_at' => $current,
+                'updated_at' => $current
             ]);
 
-            for($iProduct = 0 ; $iProduct < 5 ; $iProduct++){
-                $idProduct = DB::table('products')->insertGetId([
-                    'name' => "Product $iProduct",
-                    'category_id' => $idCategorie,
-                    'sku' => strtoupper(uniqid()),
-                    'name' => "Product $iProduct - Category $i",
-                    'description' => "Description of product $iProduct of category $i",
-                    'image' => '',
-                    'stock' => 10
+            for($x = 0 ; $x < 3 ; $x++){
+                $idChild = DB::table('categories')->insertGetId([
+                    'name' => "Subcategory $x of Category $i with id $idCategory",
+                    'description' => "Description of category $i",
+                    'category_parent_id' => $idCategory,
+                    'created_at' => $current,
+                    'updated_at' => $current
                 ]);
+
+                for($iProduct = 0 ; $iProduct < 5 ; $iProduct++){
+                    $idProduct = DB::table('products')->insertGetId([
+                        'name' => "Product $iProduct",
+                        'category_id' => $idChild,
+                        'sku' => strtoupper(uniqid()),
+                        'name' => "Product $iProduct - Category $x of $idChild",
+                        'description' => "Product $iProduct - Category $x of $idChild",
+                        'image' => '',
+                        'stock' => 10,
+                        'created_at' => $current,
+                        'updated_at' => $current
+                    ]);
+                }
             }
         }
     }
